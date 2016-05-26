a9bd8253af1a770d1fc009ddbdbeb15f5f1a7eb1
FabioPonte <fabio@erpsolutions.com.br>
2016-05-26 01:34:17 -0300

    Projeto inicial

---
+++ empretecos/Envio.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Title="Envio" Language="C#" MasterPageFile="~/Default.Master" AutoEventWireup="true" CodeBehind="Envio.aspx.cs" Inherits="empretecos.Envio" %>
+<asp:Content ID="Content_envio_1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content_envio_2" ContentPlaceHolderID="erContentPlaceHolder1" runat="server">
+    
+<div align="center">
+</asp:Label><asp:FileUpload ID="FileUploadControl" runat="server"    /></div>
+
+  <div align="center">  <asp:Button ID="Button_upload" runat="server" Text="Upload" 
+          onclick="Button_upload_Click" /> </div>
+
+          <div align="center">    <asp:GridView ID="GridView_arquivos" runat="server" 
+                  BackColor="White" BorderColor="#DEDFDE" BorderStyle="None" BorderWidth="1px" 
+                  CellPadding="4" ForeColor="Black" GridLines="Vertical">
+              <AlternatingRowStyle BackColor="White" />
+              <FooterStyle BackColor="#CCCC99" />
+              <HeaderStyle BackColor="#6B696B" Font-Bold="True" ForeColor="White" />
+              <PagerStyle BackColor="#F7F7DE" ForeColor="Black" HorizontalAlign="Right" />
+              <RowStyle BackColor="#F7F7DE" />
+              <SelectedRowStyle BackColor="#CE5D5A" Font-Bold="True" ForeColor="White" />
+              <SortedAscendingCellStyle BackColor="#FBFBF2" />
+              <SortedAscendingHeaderStyle BackColor="#848384" />
+              <SortedDescendingCellStyle BackColor="#EAEAD3" />
+              <SortedDescendingHeaderStyle BackColor="#575357" />
+                                    </asp:GridView>
+    </div>
+
+<div align="center"><asp:Label ID="Label_result_upload" runat="server" Text=""></asp:Label></div>
+</asp:Content>
---
+++ empretecos/Envio.aspx.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+using System.Collections;
+using System.IO;
+using System.Drawing;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
+
+
+
+namespace empretecos
+{
+    public partial class Envio : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                    string path = System.Configuration.ConfigurationManager.AppSettings["upload"].ToString();
+
+
+                    if (!System.IO.Directory.Exists(path)) ;
+                    {
+                        System.IO.Directory.CreateDirectory(path);
+
+                    }
+                    ArrayList listaArq = new ArrayList();
+                    System.IO.DirectoryInfo dirCliente = new System.IO.DirectoryInfo(path);
+
+
+                    foreach (System.IO.FileInfo arq in dirCliente.GetFiles())
+                    {
+
+                        listaArq.Add(arq.Name);
+
+                    }
+
+
+                    GridView_arquivos.DataSource = listaArq;
+
+                    GridView_arquivos.DataBind();
+
+
+           
+            }
+            catch (Exception ex)
+            {
+                Label_result_upload.ForeColor = Color.Red;
+                Label_result_upload.Text = "Upload status: " + ex.Message;
+
+            }
+            
+        }
+
+        protected void Button_upload_Click(object sender, EventArgs e)
+        {
+            if (FileUploadControl.HasFile)
+            {
+                try
+                {
+                    Boolean lOk = true;
+                    if (FileUploadControl.PostedFile.ContentLength > 3000000)
+                    {
+                        lOk = false;
+                        Label_result_upload.Text = "Arquivo acima de 4MB";
+                        Label_result_upload.ForeColor = Color.Red;
+                        
+
+                    }
+                    if (lOk)
+                    {
+                        string path = System.Configuration.ConfigurationManager.AppSettings["upload"].ToString();
+                        string path1 =path + @"\" + FileUploadControl.FileName;
+                        path1 = path1.Replace(" ", "_");
+
+
+                        System.IO.DirectoryInfo dirCliente = new System.IO.DirectoryInfo(path);
+                        foreach (System.IO.FileInfo arq in dirCliente.GetFiles())
+                        {
+                            
+                            if (arq.Name.ToLower().Contains(".xls") || arq.Name.ToLower().Contains(".xlsx") )
+                            {
+                                //File.Delete(arq.FullName);
+                            }
+
+
+                        }
+
+                        FileUploadControl.SaveAs(path1.Replace("+", "_"));
+                        ArrayList listaArq = new ArrayList();
+                        System.IO.DirectoryInfo dirCliente2 = new System.IO.DirectoryInfo(path);
+
+
+                        foreach (System.IO.FileInfo arq in dirCliente2.GetFiles())
+                        {
+
+                            listaArq.Add(arq.Name);
+
+                        }
+
+
+                        GridView_arquivos.DataSource = listaArq;
+
+                        GridView_arquivos.DataBind();
+
+                        Label_result_upload.ForeColor = Color.Blue;
+                        Label_result_upload.Text = "Upload realizado!";
+                        
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Label_result_upload.ForeColor = Color.Red;
+                    Label_result_upload.Text = "Upload status: " + ex.Message;
+           
+                }
+            }
+            else
+            {
+                Label_result_upload.ForeColor = Color.Red;
+                Label_result_upload.Text = "Selecione um arquivo";
+              
+            }
+        }
+    }
+}
